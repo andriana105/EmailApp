3d0117d3eb48594a203a64915db03a548bb423bc
andriana105 <andrianamarisa@gmail.com>
2021-11-23 16:52:07 +0700

    Initial Commit

---
+++ src/EmailApp/EmailApp.java
@@ -0,0 +1,10 @@
+package EmailApp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        //Todo Auto-generated method stub
+        Email email = new Email("Lily", "Lily");
+        System.out.println(email.showInfo());
+    }
+}
